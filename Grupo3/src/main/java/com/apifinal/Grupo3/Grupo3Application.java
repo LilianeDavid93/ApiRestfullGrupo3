66f1930d8874029b0788dccbd53c496b6b0443b2
LilianeDavid93 <lilianemarquesdavid@gmail.com>
2023-10-19 15:29:50 -0300

    Criando pacotes

--- Grupo3/src/main/java/com/apifinal/Grupo3/Grupo3Application.java
+++ Grupo3/src/main/java/com/apifinal/Grupo3/Grupo3Application.java
@@ -1,13 +1,19 @@
 package com.apifinal.Grupo3;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Grupo3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Grupo3Application.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper() {
+	    return new ModelMapper();
 	}
 
 }
