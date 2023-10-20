ea9baaacec1bc28421caaa7c2cdc16823845ad32
YoloDesu <be.granja@gmail.com>
2023-10-19 21:24:11 -0300

    feat: repositories

--- Grupo3/src/main/java/com/apifinal/Grupo3/repository/ClienteRepository.java
+++ Grupo3/src/main/java/com/apifinal/Grupo3/repository/ClienteRepository.java
@@ -1,5 +1,9 @@
 package com.apifinal.Grupo3.repository;
 
-public interface ClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apifinal.Grupo3.entity.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
 }
---
+++ Grupo3/src/main/java/com/apifinal/Grupo3/repository/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.apifinal.Grupo3.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apifinal.Grupo3.entity.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
